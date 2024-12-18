8a380f41539c94b85f9c589a967ca74b5b6798e1
Ryu <kpleeae@connect.ust.hk>
2024-12-18 23:37:20 +0800

    puts trap vector

--- virutal_machine/lib/trap.cpp
+++ virutal_machine/lib/trap.cpp
@@ -4,12 +4,25 @@
 #include "trap.hpp"
 
 extern bool running;
+extern Memory memory;
 extern lc_word_t reg[R_COUNT];
 
 void trap_getc()
 {
     reg[R_R0] = (lc_uint_t)getchar();
     reg[R_R0] &= 0x0F;
+}
+
+void trap_puts()
+{
+    lc_uint_t* c = memory.main_memory.data() + reg[R_R0] - 1;
+    while (*c)
+    {
+        // two char in one word
+        std::cout << (char)(*c & 0xFF);
+        std::cout << (char)(*c >> 8);
+        c++;
+    }
 }
 
 void trap_out()
@@ -32,6 +45,7 @@ void trap_halt()
 std::map<int, trap_func> trap_table = {
     {0x20, trap_getc},
     {0x21, trap_out},
+    {0x22, trap_puts},
     {0x24, trap_out_int},
     {0x25, trap_halt},
 };
