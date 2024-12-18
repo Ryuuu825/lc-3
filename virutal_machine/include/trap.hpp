3d3ca4f02ef3330aa8ae140a6edbd3c14c35d21c
Ryu <kpleeae@connect.ust.hk>
2024-12-18 23:47:10 +0800

    remove unnessary function header

--- virutal_machine/include/trap.hpp
+++ virutal_machine/include/trap.hpp
@@ -1,12 +1,9 @@
 #pragma once
 
 #include <cstdio>
+
 #include "lc_type.hpp"
 #include "register.hpp"
 #include "memory.hpp"
-
-void trap_getc();
-void trap_out();
-void trap_halt();
 
 typedef void (*trap_func)(void);
