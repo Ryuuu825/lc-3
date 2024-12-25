a43d31ad88e5aa0b680b173247b7a93e2567f37a
Ryu <kpleeae@connect.ust.hk>
2024-12-25 17:37:21 +0800

    vm

--- virutal_machine/main.cpp
+++ virutal_machine/main.cpp
@@ -25,13 +25,16 @@ int main(int argc, char** argv)
     lc_word_t instr;
     file.read(reinterpret_cast<char*>(&instr), sizeof(lc_word_t)); // starting address stored in the first word
     lc_uint_t i = instr;
+
+    reg[R_PC] = i;
+    reg[R_COND] = FL_ZRO;
+
     while (file.read(reinterpret_cast<char*>(&instr), sizeof(lc_word_t)))
     {
         memory.write(i++, instr);
     }
 
-    reg[R_PC] = 0x3000; // PC starts at 0x3000
-    reg[R_COND] = FL_ZRO;
+
 
     while(running && reg[R_PC] < 0x8000)
     {
